875c4ffdb1eb126331d770bf98f3bde88070d11a
Hafeez Hamza <hafeez1042@users.noreply.github.com>
2018-10-09 18:13:24 +0530

    test file chnage

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,8 +1,8 @@
 import { expect } from "chai";
 import validationFailMessages from "../src/const/validationFailMessages";
 import validationTypes from "../src/const/validationTypes";
+import Validator from "../src/index";
 import validationMessage from "../src/libs/validationMessage";
-import Validator from "../src/Validator";
 
 describe("String Validator", () => {
   const validator = new Validator();
